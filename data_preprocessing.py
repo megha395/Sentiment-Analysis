036ba9e054f6d269f75fe6973bc236d817e5a315
megha395 <megha240397@gmail.com>
2024-05-28 17:28:04 +0530

    preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,9 +2,9 @@ from global_imports import *
 
 #importing the training data
 imdb_data=pd.read_csv('IMDB Dataset.csv')
-# print(imdb_data.head(5))
 
-## split the dataset  
+
+## Train-Test Split................
 ## train dataset
 train_reviews=imdb_data.review[:40000]
 train_sentiments=imdb_data.sentiment[:40000]
@@ -22,7 +22,7 @@ tokenizer=ToktokTokenizer()
 stopword_list=nltk.corpus.stopwords.words('english')
 #set stopwords to english
 stop=set(stopwords.words('english'))
-print(stop)
+
 
 #Removing the noisy text
 def denoise_text(text):
@@ -42,10 +42,11 @@ def denoise_text(text):
     text= ' '.join([ps.stem(word) for word in text.split()])
     return text
 
-#Apply function on review column
 imdb_data['review']=imdb_data['review'].apply(denoise_text)
 
-#removing the stopwords
+
+
+### Stopwords Removal
 def remove_stopwords(text, is_lower_case=False):
     tokens = tokenizer.tokenize(text)
     tokens = [token.strip() for token in tokens]
@@ -56,12 +57,26 @@ def remove_stopwords(text, is_lower_case=False):
     filtered_text = ' '.join(filtered_tokens)    
     return filtered_text
 
-#Apply function on review column
 imdb_data['review']=imdb_data['review'].apply(remove_stopwords)
+
+
 
 ## Data Normalization
 norm_train_reviews=imdb_data.review[:40000]
 norm_test_reviews=imdb_data.review[40000:]
+
+
+
+### Bag of words
+#Count vectorizer for bag of words
+cv=CountVectorizer(min_df=0,max_df=1,binary=False,ngram_range=(1,3))
+#transformed train and test reviews
+cv_train_reviews=cv.fit_transform(norm_train_reviews)
+cv_test_reviews=cv.transform(norm_test_reviews)
+
+print('BOW_cv_train:',cv_train_reviews.shape)
+print('BOW_cv_test:',cv_test_reviews.shape)
+#vocab=cv.get_feature_names()-toget feature names
 
 ##### https://www.kaggle.com/code/lakshmi25npathi/sentiment-analysis-of-imdb-movie-reviews/notebook
 
